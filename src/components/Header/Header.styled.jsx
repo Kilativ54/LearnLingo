3a897328ed26ab705347f28c88abd22d61cb021b
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-14 23:58:05 +0100

    start work on styles of header

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 
 
 export const Container = styled.div`
+
+margin-left: auto;
+margin-right: auto;
   display: flex;
   align-items: center;
   justify-content: space-between;
