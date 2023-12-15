7a665cd4591c0f1249c34b7ab3ff98e170f78083
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-15 01:11:31 +0100

    update size header

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,11 +1,15 @@
 import styled from '@emotion/styled';
 
 
-export const Container = styled.div`
-
-margin-left: auto;
-margin-right: auto;
-  display: flex;
-  align-items: center;
+export const Container = styled.header`
+margin-top: 20px;
+ border-radius: 8px;
+ padding-left: 126px;
+    padding-right: 126px;
+  /* max-width:1184px; */
+  max-height: 48px;
+   display: flex;
   justify-content: space-between;
+  align-items: center;
+  z-index: 999;
 `;
