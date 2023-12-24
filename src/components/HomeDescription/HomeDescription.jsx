e88f67a18ee54a0cd426891c2c7acbbeb24d96e6
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-24 08:55:38 +0100

    cerate two sections in homePage

--- src/components/HomeDescription/HomeDescription.jsx
+++ src/components/HomeDescription/HomeDescription.jsx
@@ -1,18 +1,16 @@
-
-import { Text } from './HomeDescription.styled';
+import { ContainerHome } from "./HomeDescription.styled";
+import { SectionGetStarted } from "./HomeDescription.styled";
+import { SectionImg } from "./HomeDescription.styled";
 
 
 const HomeDescription = () => {
 
   return (
-    <>
-      <Text>
-        Are you looking for a car for travel or work? Our service is ready to
-        provide you with the best selection of vehicles for your needs.
-      </Text>
+    <ContainerHome>
+  <SectionGetStarted>Get started!</SectionGetStarted>
+  <SectionImg/>  
       
-      
-    </>
+    </ContainerHome>
   );
 };
 
--- src/components/HomeDescription/HomeDescription.styled.jsx
+++ src/components/HomeDescription/HomeDescription.styled.jsx
@@ -1,19 +1,48 @@
 import styled from '@emotion/styled';
+import BackImg from '../../img/home-page/back-img-homePage.png'
+// import BackImg from {../../}
+// export const TextWrapper = styled.div`
+//   margin-bottom: 20px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
+// export const Text = styled.p`
+//   font-size: 18px;
+//   margin-bottom: 20px;
+// `;
 
-export const TextWrapper = styled.div`
-  margin-bottom: 20px;
+// export const Image = styled.img`
+//   max-width: 100%;
+//   height: auto;
+//   margin-top: 30px;
+//   margin-bottom: 30px;
+// `;
+
+
+export const SectionGetStarted = styled.section`
+  width: 720px;
+  height: 530px;
+  border-radius: 30px;
+  background-color: rgba(248, 248, 248, 1);
+
+`;
+export const SectionImg =styled.div`
+width: 568px;
+height: 530px;
+   display: flex;
+    justify-content:center;
+   
+    background-image: url(${BackImg});
+    background-size: auto;
+    background-position:center bottom 0;
+    background-repeat: no-repeat;
+    background-size: 100%;  
+`;
+
+export const ContainerHome = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-export const Text = styled.p`
-  font-size: 18px;
-  margin-bottom: 20px;
+  gap: 24px;
 `;
 
-export const Image = styled.img`
-  max-width: 100%;
-  height: auto;
-  margin-top: 30px;
-  margin-bottom: 30px;
-`;
+   
