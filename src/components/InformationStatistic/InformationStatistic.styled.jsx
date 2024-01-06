dc401f10b8aef388deff39a1eaf2214e29eac450
Kilativ54 <vitalikechkalov@gmail.com>
2024-01-06 14:21:40 +0100

    create background section statickinfo

--- src/components/InformationStatistic/InformationStatistic.styled.jsx
+++ src/components/InformationStatistic/InformationStatistic.styled.jsx
@@ -1,10 +1,26 @@
 import styled from "@emotion/styled";
+import BackInfo from '../../img/home-page/border.png'
 
 export const SectionInformation = styled.section`
     font-family: inherit;
     width: 1312px;
     height: 116px;
     border-radius: 30px;
-    border: 1.5px solid rgba(244, 197, 80, 1);
-border-image: url('../../img/home-page/border.png') 30 round;
+    background-image: url(${BackInfo});
+    background-size: auto;
+    background-position:center bottom 0;
+    background-repeat: no-repeat;
+    background-size: 100%;  
 `       
+// export const SectionImg =styled.div`
+// width: 568px;
+// height: 530px;
+//    display: flex;
+//     justify-content:center;
+   
+//     background-image: url(${BackImg});
+//     background-size: auto;
+//     background-position:center bottom 0;
+//     background-repeat: no-repeat;
+//     background-size: 100%;  
+// `;
