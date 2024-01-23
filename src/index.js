bdf1548054d972c274e108b702c5cf39ab6392bf
Kilativ54 <vitalikechkalov@gmail.com>
2024-01-23 22:15:37 +0100

    fixing index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistor, store } from '../src/redux/store';
+import 'modern-normalize/modern-normalize.css';
+import '../src/components/styles/loader.css';
+import '../src/components/styles/variables.css';
+import './styles/fonts.css';
 import './index.css';
+import '../src/components/styles/scroll.css';
+// import './helpers/scroll';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <BrowserRouter basename="/LearnLingo">
-    <App />
-  </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/LearnLingo">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
