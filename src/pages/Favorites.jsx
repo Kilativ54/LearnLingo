1e1bacc36385214af2675e6f2de00dfe1e386039
Kilativ54 <vitalikechkalov@gmail.com>
2024-02-18 22:12:37 +0100

    add logic to favorite pages

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,23 @@
+import { useSelector } from 'react-redux';
+
+import CarsList from 'components/CarsList/CarsList';
+import NoCarsSection from 'components/NoCarsSection/NoCarsSection';
+import Title from 'components/Title/Title';
+import { selectFavorite } from '../redux/favorite/favoriteSelectors';
+
 const Favorites = () => {
-    return (
-     <h1>Favor</h1>
-              
-      
-    );
-  };
-  
-  export default Favorites;
+  const { favoriteCars } = useSelector(selectFavorite);
+
+  return (
+    <section>
+      <Title title="Your Favorite Cars" />
+      {favoriteCars.length > 0 ? (
+        <CarsList carsArray={favoriteCars} />
+      ) : (
+        <NoCarsSection />
+      )}
+    </section>
+  );
+};
+
+export default Favorites;
