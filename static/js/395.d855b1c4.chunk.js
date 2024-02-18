f2bdcc064c746bcad4bb8858bc9f9e1f73d840f3
Kilativ54 <113536851+Kilativ54@users.noreply.github.com>
2024-02-18 21:14:25 +0000

    Deploying to gh-pages from @ Kilativ54/LearnLingo@1e1bacc36385214af2675e6f2de00dfe1e386039 🚀

--- static/js/395.d855b1c4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkLearnLingo=self.webpackChunkLearnLingo||[]).push([[395],{7395:function(n,e,r){r.r(e);var u=r(184);e.default=function(){return(0,u.jsx)("h1",{children:"Favor"})}}}]);
-//# sourceMappingURL=395.d855b1c4.chunk.js.map
